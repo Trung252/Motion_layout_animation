ec5283ac39940ca54dc929851b758118a9c73bdf
Phạm Kiên Trung <pkt02052000@gmail.com>
2021-09-23 11:46:51 +0700

    finish baby motion

---
+++ app/src/main/java/com/example/motionlayoutanimation/Baby_motion.java
@@ -0,0 +1,14 @@
+package com.example.motionlayoutanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Baby_motion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baby_motion);
+    }
+}
