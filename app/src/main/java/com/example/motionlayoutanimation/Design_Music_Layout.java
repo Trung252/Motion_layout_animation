9b4bbe99dc651c26c6c51a2ea0cb7eb030a99128
Phạm Kiên Trung <pkt02052000@gmail.com>
2021-09-25 20:07:34 +0700

    finish Music motion layout

---
+++ app/src/main/java/com/example/motionlayoutanimation/Design_Music_Layout.java
@@ -0,0 +1,14 @@
+package com.example.motionlayoutanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Design_Music_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_design_music_layout);
+    }
+}
